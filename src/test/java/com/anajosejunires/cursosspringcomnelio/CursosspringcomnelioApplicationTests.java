ecd1cacc96b02fe8ce07a9581c061c71bc9087a7
AnaSantana <anapedra.mil@gmail.com>
2022-09-25 12:21:41 -0300

    Initial commit

---
+++ src/test/java/com/anajosejunires/cursosspringcomnelio/CursosspringcomnelioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anajosejunires.cursosspringcomnelio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursosspringcomnelioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
